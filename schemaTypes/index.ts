a985ea9b26b5229ec22aa7514f93fccedb07a7de
Okode100 <alfakings73@gmail.com>
2025-04-30 14:14:21 +0300

    create schema

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,6 +1,7 @@
 import {eventType} from './eventType'
 import {artistType} from './artistType'
+import {venueType} from './venueType' 
 
-export const schemaTypes = [eventType,artistType]
+export const schemaTypes = [eventType,artistType,venueType] 
 
 
---
+++ schemaTypes/venueType.ts
@@ -0,0 +1,14 @@
+import {defineField, defineType} from 'sanity'
+
+export const venueType = defineType({
+    name: 'venue',
+    title: 'Venue',
+    type: 'document',
+    fields: [
+        defineField({
+            name:'name',
+            type:'string',
+        }),
+    ],
+
+})
