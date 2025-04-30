40583e5dc5c9776edecf5f7c75d94631fdafcf75
Okode100 <alfakings73@gmail.com>
2025-04-30 23:50:04 +0300

    create a custom input from sanity/ui

--- schemaTypes/eventType.ts
+++ schemaTypes/eventType.ts
@@ -1,5 +1,6 @@
 import {defineField, defineType} from 'sanity'
 import {CalendarIcon} from '@sanity/icons'
+import {DoorsOpenInput} from './components/DoorsOpenInput'
 
 export const eventType = defineType({
   name: 'event',
@@ -47,6 +48,9 @@ defineField({
         description: 'Number of minutes before the start time for admission',
         initialValue:60,
         group: 'details',
+        components: {
+          input: DoorsOpenInput,
+        }
         
 
     }),
